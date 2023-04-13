b7b0d75f9af6c6b035e31e58a53c76da35ee2b13
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-04-13 07:59:10 -0700

    created db folder with query, schema, and seeds sql files. Wrote code for human resource table and employee database in scheme file and wrote test data in seeds file

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS human_resources_db;
+-- Creates the "human_resources_db" database --
+CREATE DATABASE human_resources_db;
+
+-- Makes it so all of the following code will affect human_resources_db --
+USE human_resources_db;
+
+CREATE TABLE employees (
+    id INT Not NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(150) NOT NULL,
+    last_name VARCHAR(150) NOT NULL,
+    title VARCHAR(250) NOT NULL,
+    department VARCHAR(250) NOT NULL,
+    salary INT NOT NULL,
+    manager
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees(id, first_name, last_name, title, department, salary, manager)
+VALUES  (1, "Sponge", "Bob", "Sales Person", Sales, 500000, Eugene Krabs)
+        (2, "Eugene", "Krabs", "Sales Lead", Sales, 100000)
+        (3, "Patrick", "Star", "Sales Person", Sales, 300000, Eugene Krabs)
